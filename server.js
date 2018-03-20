a5158540e28a37d2bb96e54cdae09dac550548cd
Nissrine <anissrine95@hotmail.fr>
2018-03-21 00:21:00 +0100

    feat(api): add API on port 9292

--- server.js
+++ server.js
@@ -2,31 +2,50 @@ const {getBrands} = require('node-car-api');
 const {getModels} = require('node-car-api');
 var fs = require('fs');
 
+//API configuration
+var express = require('express'), 
+app = express(),
+port = 9292;
+
 //Test function
 async function Brands() {
  var brands = await getBrands();
  console.log(brands);
 }
-Brands();
+//Brands();
 
 //Import the data into a json file
 async function populate () {
-   const models = await getModels('PEUGEOT');
-   var toString = "";
-   var id = 0;
+   const brands = await getBrands();
+   brands.forEach(async brand => {
+	const models = await getModels(brand);
+	var toString = "";
+	var id = 0;
+ 
+	  models.forEach(function(element) {
+ 
+		 toString += '{ "index":{ "_index": "suv", "_type" : "suv", "_id": "'+id+'"}}\n';
+		 toString += JSON.stringify(element)+"\n";
+		 id++;
+	 });
+	 fs.writeFile('data.json', toString, async (err) => {
+		if (err) throw err;
 
-     models.forEach(function(element) {
-
-  		toString += '{ "index":{ "_index": "suv", "_type" : "suv", "_id": "'+id+'"}}\n';
-		toString += JSON.stringify(element)+"\n";
-		id++;
-	});
-
-  fs.writeFile('data.json', toString, function (err) {
-    if (err) throw err;
-  });
+   });
+	  });
+  
  }
+populate();
 
- populate();
+//API route 
+app.get('/', (req,res) => {
+	res.send('Test'); //Ok 
+});
+app.listen(port);
+
+console.log("Server launch on port " + port);
+
+
+ 
 
 
